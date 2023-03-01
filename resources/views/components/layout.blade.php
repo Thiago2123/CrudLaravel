4298c84bbacba36191a2817d966e8cadf7b7f07c
Thiago Meneghetti Rossi <thi_meneghetti@hotmail.com>
2023-03-01 14:45:48 -0300

    Bootstrap
    Inserindo o bootstrap no projeto

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,6 +3,12 @@
 
     <head>
         <meta charset="UTF-8">
+
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0/css/bootstrap.min.css">
+        <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+        <script src="../lib/js/bootstrap.min.js"></script>
+
+
         <title>{{ $titulo }}</title>
     </head>
     <body>
--- resources/views/machines/index.blade.php
+++ resources/views/machines/index.blade.php
@@ -1,15 +1,18 @@
 <x-layout titulo="Listar Maquinas"> 
-    
-    <a href="{{ route('maquinas.create') }}">Cadastrar</a>
+
+<div class="d-flex flex-row-reverse bd-highlight p-4">
+
+    <a class="btn btn-success p-2 bd-highlight" href="{{ route('maquinas.create') }}">Cadastrar</a>
+</div >
 
     <h1>Listar as maquinas</h1>
     @include('components/mensagem')
-    <table>
+    <table class="table table-hover table-dark">
         <thead>
             <tr>
-                <th>ID</th>
-                <th>Nome</th>
-                <th>Ações</th>
+                <th class="col-1">ID</th>
+                <th class="col-9">Nome</th>
+                <th class="col-2">Ações</th>
             </tr>
         </thead>
         <tbody>
@@ -17,17 +20,13 @@
                 <tr>
                     <td>{{$machine->id}}</td>
                     <td>{{$machine->nome}}</td>
-                    <td style="display: flex">
-                        <button type="button"><a href="{{ route('maquinas.show', $machine->id)}}">Visualizar</a></button>
-                    </td>
-                    <td>
-                        <button type="button"><a href="{{ route('maquinas.edit', $machine->id)}}">Editar</a></button>
-                    </td>
-                    <td>
-                        <form action="{{ route('maquinas.destroy', $machine->id) }}" method="POST">
+                    <td class="">
+                        <a class="btn btn-primary" href="{{ route('maquinas.show', $machine->id)}}">Visualizar</a>
+                        <a class="btn btn-warning" href="{{ route('maquinas.edit', $machine->id)}}">Editar</a>
+                        <form style="display:contents" action="{{ route('maquinas.destroy', $machine->id) }}" method="POST">
                             @csrf
                             @method('DELETE')
-                            <button type="submit">Apagar</button>
+                            <button type="submit" class="btn btn-danger">Apagar</button>
                         </form>
                     </td>
                 </tr>
